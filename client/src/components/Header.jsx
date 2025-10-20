603ef108ace3499a6380252d9cc5f846af415ffb
Kaia <kaia.bryant@yahoo.com>
2025-10-20 11:56:57 -0400

    rerouted home image

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,28 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../assets/styles/Header.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../assets/styles/Header.css";
 
 export default function Header() {
     return (
         <div className="full-header">
             {/* Upperhead banner */}
             <div className="upper-header-wrap">
-                <p>Grow. Play. Every Day!</p>
-            </div>
-            <div className="uhead__title">
-                <Link to="/">TRELLIS</Link>
-            </div>
-            <div className="uhead__right">
-                <p>Grow Your Own Paradise</p>
+                <div className="uhead__left">
+                    <p>Grow. Play. Every Day!</p>
+                </div>
+
+                <div className="uhead__title">
+                    <Link to="/">TRELLIS</Link>
+                </div>
+
+                <div className="uhead__right">
+                    <p>Grow Your Own Paradise</p>
+                </div>
             </div>
 
-            {/* Header Section */}
+            {/* Main Navbar */}
             <header className="header">
                 <nav className="navbar">
-                    {/* Use React Router links instead of hrefs */}
                     <Link to="/shop">Shop All</Link>
                     <Link to="/contact">Free Plants</Link>
-                    {/* <Link to="/guide">How To Grow</Link> */}
                 </nav>
             </header>
         </div>
